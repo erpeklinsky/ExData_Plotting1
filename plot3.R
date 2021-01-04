ea1e17219c6104e315afda0857c9f9d2a7457710
erpeklinsky <erpeklinsky@gmail.com>
2021-01-04 15:27:42 -0500

    coding for plot3

---
+++ plot3.R
@@ -0,0 +1,32 @@
+##Read data
+Data <- read.delim("household_power_consumption.txt", header = TRUE, sep = ";", 
+                   na.strings = "?", colClasses = c("character","character",
+                  "numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+
+##subset data based on dates needed
+subsetData <- Data[Data$Date %in% c("1/2/2007", "2/2/2007"),]
+
+##Format date to type date 
+subsetData$Date <- as.Date(subsetData$Date, "%d/%m/%Y")
+
+##create a combined date and time column and add to the subset data 
+DateTime <- paste(subsetData$Date, subsetData$Time)
+DateTime <- setNames(DateTime, "DateTime")
+subsetData <- cbind(DateTime, subsetData)
+subsetData$DateTime <- as.POSIXct(DateTime)
+
+##Plot3
+
+plot(subsetData$Sub_metering_1~subsetData$DateTime, type="l", col = "black", 
+     ylab="Energy sub metering", xlab="")
+
+points(subsetData$Sub_metering_2~subsetData$DateTime, type="l", col = "red")
+
+points(subsetData$Sub_metering_3~subsetData$DateTime, type="l", col = "blue")
+
+legend("topright", col = c("black", "red", "blue"), lwd = c(1,1,1), 
+       c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
